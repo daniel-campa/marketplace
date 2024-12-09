8d876ce6a1be3810f86872d8c8e4045795daa81e
daniel-campa <danielcampa4@gmail.com>
2024-12-09 13:58:33 -0500

    Updated dashboard

--- marketplace_scraper.py
+++ marketplace_scraper.py
@@ -134,10 +134,10 @@ while True:
         out_df = listings_df.sort_values(['price'])
 
         print(
-            tabulate(out_df, headers='keys', tablefmt='psql', showindex=False, maxcolwidths=[60, 6, 7, 8, 17, 5, 70])
+            tabulate(out_df.drop(['image'], axis=1), headers='keys', tablefmt='psql', showindex=False, maxcolwidths=[60, 6, 7, 8, 17, 5, 70])
         )
 
-        out_df.to_html(content_path, index=False)
+        out_df.to_html(content_path, index=False, escape=False)
 
 
         repo = git.Repo(repo_path)
